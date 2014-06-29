270da87569fe773c84dbdc31071d3ea737b1314a
Kenni Bawden <kenni.bawden@gmail.com>
2014-06-29 12:20:12 +1000

    Added list of contents
    links don't work yet

--- js/demo.js
+++ js/demo.js
@@ -1,5 +1,11 @@
 angular.module('demo', []).
     value('title', 'TeachUp').
-    controller('demoCtrl', function($scope, title) {
+    controller('demoCtrl', function($scope, $http, title) {
         $scope.title = title;
+        $scope.content = 
+        $http.get('curriculumA.json')
+            .then(function(res) {
+                $scope.contents = res.data.content;
+                $scope.updates = res.data.updates;
+            });
     });
